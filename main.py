b43ae7fe7af4e19e86e8ccda7bc4b01c84f5405c
jhChang1216 <wnsk1216@ajou.ac.kr>
2022-12-11 03:05:18 +0900

    2022-12-11

--- main.py
+++ main.py
@@ -8,20 +8,25 @@ from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from datetime import datetime
+import numpy as np
+import math
+from statsmodels.formula.api import ols
+import seaborn as sns
+import scipy.stats
 
 
-def preprocessing_df(df, column=0, MA_condition=0, window = 6):
+def preprocessing_df(df, column=0, MA_condition=0, window=6):
     df = df.T
     df = df.iloc[1:, column]
     index_list = list()
     item_list = list()
     try:
         df.index = pd.to_datetime(df.index)
-    except: #환율데이터는 추가적인 전처리 과정 필요
+    except:  # 환율데이터는 추가적인 전처리 과정 필요
         for idx in df.index:
             year = str(idx[0:4])
             month = str(idx[4:6])
-            tmp = pd.to_datetime(year+'-'+month)
+            tmp = pd.to_datetime(year + '-' + month)
             index_list.append(tmp)
         df.index = index_list
         for item in df:
@@ -38,88 +43,92 @@ def preprocessing_df(df, column=0, MA_condition=0, window = 6):
         df = df.rolling(window=window).mean()  # 이동평균 해볼까??
     return df
 
+
 def sorting_index(df_list):
     tmp = df_list[0]
     for i, item in enumerate(df_list):
-        if(item.index[0] > tmp.index[0]):
+        if (item.index[0] > tmp.index[0]):
             tmp = item
     return tmp
+
 
 def sorting_index_min(df_list):
     tmp = df_list[0]
     for i, item in enumerate(df_list):
-        if(item.index[0] < tmp.index[0]):
+        if (item.index[0] < tmp.index[0]):
             tmp = item
     return tmp
 
+
 def replace_NAN_to_column_mean(df, column):
-    df[column] = df[column].fillna(method = 'ffill')
+    df[column] = df[column].fillna(method='ffill')
     df[column] = df[column].fillna(0)
     return df
 
-def load_data():
 
-   #path = 'C:/Users/wnsk1/OneDrive/바탕 화면/pythonProject1/datasets' #집 PC
+def load_data():
+    #path = 'C:/Users/wnsk1/OneDrive/바탕 화면/pythonProject1/datasets'  # 집 PC
     path = 'C:/projects/pythonProject2/Sogang_Bigdata/datasets' #숙소 PC
+
     df_list = []
 
-    oil = pd.read_excel(path+'/주유소_평균_판매가격_20221109032912.xlsx')
+    oil = pd.read_excel(path + '/주유소_평균_판매가격_20221109032912.xlsx')
 
     gasoline = preprocessing_df(oil, column=0)
-    #deisel = preprocessing_df(oil, column=3)
+    # deisel = preprocessing_df(oil, column=3)
 
     df_list.append(gasoline)
 
-    interest = pd.read_excel(path+'/예금은행_대출금리_신규취급액_기준__20221109042933.xlsx')
+    interest = pd.read_excel(path + '/예금은행_대출금리_신규취급액_기준__20221109042933.xlsx')
     interest = preprocessing_df(interest)
     df_list.append(interest)
 
-    m_currency = pd.read_excel(path+'/본원통화_구성내역_평잔__원계열__20221109045023.xlsx')
+    m_currency = pd.read_excel(path + '/본원통화_구성내역_평잔__원계열__20221109045023.xlsx')
     m_currency = preprocessing_df(m_currency)
     df_list.append(m_currency)
 
-    current_account = pd.read_excel(path+'/국제수지_20221109043720.xlsx')
+    current_account = pd.read_excel(path + '/국제수지_20221109043720.xlsx')
     current_account = preprocessing_df(current_account)
     df_list.append(current_account)
 
-    PPI = pd.read_excel(path+'/국내공급물가지수_20221109033416.xlsx')
+    PPI = pd.read_excel(path + '/국내공급물가지수_20221109033416.xlsx')
     PPI = preprocessing_df(PPI, 2)
     df_list.append(PPI)
 
-    spot_rate = pd.read_excel(path+'/환율통계.xlsx')
+    spot_rate = pd.read_excel(path + '/환율통계.xlsx')
     spot_rate_dollor = preprocessing_df(spot_rate, 0)
     df_list.append(spot_rate_dollor)
 
-    CLI = pd.read_excel(path+'/경기종합지수_2015100__구성지표_시계열__10차__20221113132427.xlsx')
+    CLI = pd.read_excel(path + '/경기종합지수_2015100__구성지표_시계열__10차__20221113132427.xlsx')
     CLI = preprocessing_df(CLI, 1)
     df_list.append(CLI)
 
-    house = pd.read_excel(path+'/행정구역별_아파트거래현황_20221113140133.xlsx')
+    house = pd.read_excel(path + '/행정구역별_아파트거래현황_20221113140133.xlsx')
     house = preprocessing_df(house)
     df_list.append(house)
 
-    house_price = pd.read_excel(path+'/아파트_규모별_매매_실거래가격지수_20221126054706.xlsx')
+    house_price = pd.read_excel(path + '/아파트_규모별_매매_실거래가격지수_20221126054706.xlsx')
     house_price = preprocessing_df(house_price)
     df_list.append(house_price)
 
-    kospi = pd.read_excel(path+'/코스피_200_지수_20221126055840.xlsx')
+    kospi = pd.read_excel(path + '/코스피_200_지수_20221126055840.xlsx')
     kospi = preprocessing_df(kospi)
     df_list.append(kospi)
 
-    deposit_rate = pd.read_excel(path+'/예금은행_수신금리_신규취급액_기준__20221126060504.xlsx')
+    deposit_rate = pd.read_excel(path + '/예금은행_수신금리_신규취급액_기준__20221126060504.xlsx')
     deposit_rate = preprocessing_df(deposit_rate)
     df_list.append(deposit_rate)
 
-    gold = pd.read_csv(path+'/금 선물 내역.csv')
+    gold = pd.read_csv(path + '/금 선물 내역.csv')
     gold_index = gold['날짜']
     gold = gold['종가']
     tmp_index = list()
     for item in gold_index:
-        item = item.replace(' ','')
+        item = item.replace(' ', '')
         tmp_index.append(datetime.strptime(item, '%Y-%m-%d'))
     tmp_list = list()
     for item in gold:
-        tmp_list.append(float(item.replace(',','')))
+        tmp_list.append(float(item.replace(',', '')))
     gold = pd.DataFrame(tmp_list)
     gold.index = tmp_index
     gold = gold.loc[::-1]
@@ -127,31 +136,36 @@ def load_data():
     new_gold_index = list()
     for i in range(len(tmp_index)):
         if gold.index[i].timetuple().tm_mday == 1:
-            new_gold.append(gold.iloc[i,0])
+            new_gold.append(gold.iloc[i, 0])
             new_gold_index.append(gold.index[i])
     gold = pd.DataFrame(new_gold)
     gold.index = new_gold_index
     df_list.append(gold)
 
-    population = pd.read_excel(path+'/대한민국인구조사.xls')
+    population = pd.read_excel(path + '/대한민국인구조사.xls')
     population = preprocessing_df(population)
     population = population.fillna(method='ffill')
     df_list.append(population)
 
-    sin_risk = pd.read_excel(path+'/신용위험.xlsx')
+    sin_risk = pd.read_excel(path + '/신용위험.xlsx')
     sin_risk = preprocessing_df(sin_risk)
     df_list.append(sin_risk)
 
-    CPI = pd.read_excel(path+'/소비자물가지수_2020100__20221109032835.xlsx')
+    GDP = pd.read_excel(path + '/GDP.xls')
+    GDP = preprocessing_df(GDP, 1) #실질경제성장률
+    df_list.append(GDP)
+
+    CPI = pd.read_excel(path + '/소비자물가지수_2020100__20221109032835.xlsx')
     CPI = preprocessing_df(CPI, 0)
     df_list.append(CPI)
 
     standard_time = sorting_index(df_list).index[0]
     df_list = list(map(lambda x: x[standard_time:], df_list))
     df_list[6].index = df_list[0].index
-    #경기종합지수 인덱스가 좀 이상하니까 바꿔주자
+    # 경기종합지수 인덱스가 좀 이상하니까 바꿔주자
 
     return df_list
+
 
 def scaling_data(df, target):
     tmp_df = df
@@ -159,26 +173,28 @@ def scaling_data(df, target):
     fitted = min_max_scaler.fit(df)
     tmp = min_max_scaler.transform(df)
     df = pd.DataFrame(tmp, columns=df.columns, index=list(df.index.values))
-    #df[target] = tmp_df[target] 그냥 target도 같이 스케일링 해버리자
+    # df[target] = tmp_df[target] 그냥 target도 같이 스케일링 해버리자
     return df
 
-def seperate_train_test(df, target, base, predict_gap):
-    #데이터 분리
-    #index 166 = 2019-12-01
-    #print(df.index.values[base])
 
-    #min_index_next = sorting_index_min(df_list).index[predict_gap]
+def seperate_train_test(df, target, base, predict_gap):
+    # 데이터 분리
+    # index 166 = 2019-12-01
+    # print(df.index.values[base])
+
+    # min_index_next = sorting_index_min(df_list).index[predict_gap]
     train_end = df.index.values[base]
-    train_end_y = df.index.values[base+predict_gap]
+    train_end_y = df.index.values[base + predict_gap]
 
     X_train = df.loc[:train_end, df.columns != target]
     X_test = df.loc[train_end:, df.columns != target]
-    #X_train = df.loc[:train_end]
-    #X_test = df.loc[train_end:]
+    # X_train = df.loc[:train_end]
+    # X_test = df.loc[train_end:]
     y_train = df.loc[df.index.values[predict_gap]:train_end_y, target]
     y_test = df.loc[train_end_y:, target]
 
     return X_train, X_test, y_train, y_test
+
 
 def drop_column(X_train, X_test, column_list):
     drop_list = column_list
@@ -186,21 +202,23 @@ def drop_column(X_train, X_test, column_list):
     X_test = X_test.drop(drop_list, axis=1)
     return X_train, X_test
 
+
 def step_backward(X_train, X_test, y_train):
     BIC_list = list()
-    while(True):
+    while (True):
         x = sm.add_constant(X_train)
         model = sm.OLS(y_train.values, x).fit()
 
-        #print("BIC : ", model.bic)
+        # print("BIC : ", model.bic)
         BIC_list.append(model.bic)
         model.pvalues = model.pvalues.drop('const')
         max_pval_idx = model.pvalues.idxmax()
         if model.pvalues[max_pval_idx] > 5.0e-02:
-            print(max_pval_idx,' dropped.')
+            print(max_pval_idx, ' dropped.')
             X_train, X_test = drop_column(X_train, X_test, [max_pval_idx])
         else:
             return X_train, X_test, BIC_list
+
 
 def predict_data(model_list, X_train, y_train, X_test, predict_gap):
     y_hat_list = list(0 for i in range(len(model_list)))
@@ -214,6 +232,7 @@ def predict_data(model_list, X_train, y_train, X_test, predict_gap):
 
     return y_hat_list
 
+
 def display_prediction(model_list, model_dict, X_train, X_test, target, y_test, y_hat_list, predict_gap):
     inv_model_dict = {v: k for k, v in model_dict.items()}
 
@@ -222,12 +241,13 @@ def display_prediction(model_list, model_dict, X_train, X_test, target, y_test, 
     plt.plot(y_test, label='real data')
     plt.plot(df.loc[X_train.index[0]:X_test.index[0], target])
     plt.legend()
-    plt.xticks(rotation = 45)
+    plt.xticks(rotation=45)
     plt.title(f'{target} prediction using data from {predict_gap} month ago')
     plt.xlabel('year-month')
     plt.ylabel(f'{target}')
     plt.grid()
     plt.show()
+
 
 def display_BIC(BIC_list):
     plt.scatter(range(len(BIC_list)), BIC_list)
@@ -238,22 +258,29 @@ def display_BIC(BIC_list):
     plt.grid()
     plt.show()
 
+
 if __name__ == '__main__':
 
     df_list = load_data()
-    df = pd.concat([df_list[0], df_list[1], df_list[2], df_list[3], df_list[4], df_list[5], df_list[6], df_list[7], df_list[8], df_list[9], df_list[10], df_list[11], df_list[12], df_list[13], df_list[14]], axis=1)
-    df.columns = ['gasoline', 'loan_rate', 'currency_vol', 'current_account', 'PPI', 'Won/dollar_rate', 'CLI', 'real_estate(volume)', 'real_estate(price)', 'kospi200', 'deposit_rate', 'gold', 'population','dept','CPI']
+    df = pd.concat(
+        [df_list[0], df_list[1], df_list[2], df_list[3], df_list[4], df_list[5], df_list[6], df_list[7], df_list[8],
+         df_list[9], df_list[10], df_list[11], df_list[12], df_list[13], df_list[14], df_list[15]], axis=1)
+    df.columns = ['gasoline', 'loan_rate', 'currency_vol', 'current_account', 'PPI', 'Won/dollar_rate', 'CLI',
+                  'real_estate(volume)', 'real_estate(price)', 'kospi200', 'deposit_rate', 'gold', 'population', 'dept',
+                  'GDP','CPI']
     print(df)
 
     for item in df.columns:
         df = replace_NAN_to_column_mean(df, item)
     print(df)
 
-    target = 'Won/dollar_rate'
+    raw_df = df
+
+    target = 'kospi200'
     # 예측하고자 하는 컬럼을 지정(부동산, 수신금리, 달러환율, 코스피, 금)
 
     df = scaling_data(df, target)
-    #target을 제외한 나머지 컬럼(독립변수)들을 min max 스케일링한다.
+    # target을 제외한 나머지 컬럼(독립변수)들을 min max 스케일링한다.
     print(df)
 
     # tmp_df = df
@@ -268,43 +295,76 @@ if __name__ == '__main__':
     RF_MSE_list = list()
     MSE_list = list([Linear_MSE_list, MLP_MSE_list, RF_MSE_list])
 
-    predict_range = 12
+    predict_range = 3
 
     model_list = [Ridge(), ann.MLPRegressor(hidden_layer_sizes=(200, 200, 200)), RandomForestRegressor()]
-    model_dict = {'Ridge': model_list[0],'MLP': model_list[1], 'Randomforest': model_list[2]}
+    model_dict = {'Ridge': model_list[0], 'MLP': model_list[1], 'Randomforest': model_list[2]}
     inv_model_dict = {v: k for k, v in model_dict.items()}
 
-    for predict_gap in range(1,predict_range+1):
-        X_train, X_test, y_train, y_test = seperate_train_test(df.iloc[:int(len(df) * 0.7)], target,int(len(df) * 0.7 * 0.8), predict_gap)
+    X_final = pd.DataFrame()
+    y_final = pd.DataFrame()
+
+    rf_col = pd.DataFrame()
+
+
+    for predict_gap in range(1, predict_range + 1):
+        # X_train, X_test, y_train, y_test = seperate_train_test(df.iloc[:int(len(df) * 0.7)], target,int(len(df) * 0.7 * 0.8), predict_gap)
         # 검정용
-        X_train, X_test, y_train, y_test = seperate_train_test(df, target,int(len(df) * 0.7), predict_gap)
+        #X_train, X_test, y_train, y_test = seperate_train_test(df, target,int(len(df) * 0.7), predict_gap)
         # 시험용
+        X_train, X_test, y_train, y_test = seperate_train_test(df, target, int(len(df) * 0.985), predict_gap)
+        # 예측용
 
         x = sm.add_constant(X_train)
         model = sm.OLS(y_train.values, x).fit()
-        print(model.summary())
+        #print(model.summary())
 
-        X_train, X_test, BIC_list = step_backward(X_train, X_test, y_train)
-        #display_BIC(BIC_list)
+
+        # X_train, X_test = drop_column(X_train, X_test, ['currency_vol', 'current_account', 'PPI', 'CLI',
+        #           'real_estate(volume)', 'real_estate(price)', 'population', 'dept',
+        #           'GDP'])
+        # 금 변수 선택
+
+        X_train, X_test = drop_column(X_train, X_test, ['real_estate(volume)', 'gold', 'population', 'dept',
+         'GDP'])
+        # 코스피200 변수 선택
+
+        # X_train, X_test = drop_column(X_train, X_test,['gasoline', 'current_account', 'PPI', 'Won/dollar_rate', 'CLI',
+        #                                                'gold', 'dept'])
+        # 부동산매매가격 변수 선택
+
+        #X_train, X_test = drop_column(X_train, X_test,['gasoline','current_account', 'PPI', 'CLI',
+                  # 'real_estate(volume)', 'real_estate(price)', 'gold', 'population', 'dept',
+                  # 'GDP'])
+        # 원/달러환율 변수 선택
+
+
+        #sns.heatmap(data=X_train.corr(), annot=True, fmt='.2f', linewidths=.5, cmap='Blues')
+        #plt.show()
+
+
+        #X_train, X_test, BIC_list = step_backward(X_train, X_test, y_train)
+        X_final = X_test
+        y_final = y_test
+        # display_BIC(BIC_list)
 
         x = sm.add_constant(X_train)
         model = sm.OLS(y_train.values, x).fit()
-        print(model.summary())
-
-        # model_list = [LinearRegression(), ann.MLPRegressor(hidden_layer_sizes=(200, 200, 200)), RandomForestRegressor()]
-        # model_dict = {'LinearRegression': model_list[0], 'MLPRegeressor': model_list[1], 'Randomforest': model_list[2]}
+        # print(model.summary())
 
         y_hat_list = predict_data(model_list, X_train, y_train, X_test, predict_gap)
-        display_prediction(model_list, model_dict, X_train, X_test, target, y_test, y_hat_list, predict_gap)
+        # display_prediction(model_list, model_dict, X_train, X_test, target, y_test, y_hat_list, predict_gap)
 
-        #inv_model_dict = {v: k for k, v in model_dict.items()}
+        # inv_model_dict = {v: k for k, v in model_dict.items()}
         for i, y_hat in enumerate(y_hat_list):
             MSE = mean_squared_error(y_hat, y_test)
             MSE_list[i].append(MSE)
             print(f'{inv_model_dict[model_list[i]]}s {MSE}')
 
+        rf_col = X_train
+
     for i, MSE in enumerate(MSE_list):
-        plt.plot(range(1, predict_range + 1), MSE, label = inv_model_dict[model_list[i]])
+        plt.plot(range(1, predict_range + 1), MSE, label=inv_model_dict[model_list[i]])
     plt.title(f'MSE according to {target} prediction period(month)')
     plt.xlabel('month')
     plt.ylabel('MSE')
@@ -312,5 +372,41 @@ if __name__ == '__main__':
     plt.grid()
     plt.show()
 
-    # #print(model_list[0].predict(X_test)[-predict_gap]*100)
-    # #마지막 관측 데이터를 바탕으로 다음 번째의 CPI를 예측한다.
+    Ridge_coef = np.array(model_list[0].coef_)
+    # MLP_coef = np.array(model_list[1].coefs_)
+    # RF_coef = np.array(model_list[2].)
+    coef_list = list([Ridge_coef])
+
+    now_num = -1
+    # 예측하고자 하는 시간(인덱스) ex- 0이면 test 데이터에서 첫번째인 2017년 11월 데이터)
+    lastest_data = np.array(X_final.iloc[now_num, :])
+    # 가장 최신인(2022-11) 예측을 위한 독립변수들. predict_gap 이후 예측을 위해
+    print(X_final.iloc[now_num, :])
+    now = y_final[now_num]
+
+    Ridge_pred = model_list[0].predict(lastest_data.reshape(1, -1))[-1]
+    MLP_pred = model_list[1].predict(lastest_data.reshape(1, -1))[-1]
+    RF_pred = model_list[2].predict(lastest_data.reshape(1, -1))[-1]
+    Ridge_return = (Ridge_pred - now) / now
+    MLP_return = (MLP_pred - now) / now
+    RF_return = (RF_pred - now) / now
+    print('현재 : ', now)
+    print('\n릿지 예측 : ', Ridge_pred)
+    print('인공신경망 예측 : ', MLP_pred)
+    print('랜덤포레스트 예측 : ', RF_pred)
+
+    asset_dict = {'gold': '금', 'real_estate(price)': '서울 부동산', 'kospi200': '국내주식', 'Won/dollar_rate': '달러'}
+
+    print(f'\n지금 {asset_dict[target]} 매입하면 {predict_range}개월 뒤')
+
+    print(f'(릿지회귀) 예측 수익률 : ', round(Ridge_return * 100), '%')
+    print(f'(인공신경망) 예측 수익률 : ', round(MLP_return * 100), '%')
+    print(f'(랜덤포레스트) 예측 수익률 : ', round(RF_return * 100), '%')
+
+    returns = raw_df[['gold', 'real_estate(price)', 'kospi200', 'Won/dollar_rate']].pct_change()
+    vol_gold = returns['gold'].std() * math.sqrt(250)
+    vol_real_estate = returns['real_estate(price)'].std() * math.sqrt(250)
+    vol_kospi = returns['kospi200'].std() * math.sqrt(250)
+    vol_dollar = returns['Won/dollar_rate'].std() * math.sqrt(250)
+    covs = returns.cov()
+    print(vol_dollar)
